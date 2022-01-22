4fff0a9563bd88d35a1b5dfaa5da3d8118321b19
katharina <katharina.sachs@seerene.com>
2022-01-22 18:03:55 +0100

    data integration

--- src/main/java/de/freerider/app/AppApplication.java
+++ src/main/java/de/freerider/app/AppApplication.java
@@ -1,6 +1,6 @@
 package de.freerider.app;
 
-import de.freerider.datamodel.Customer;
+import de.freerider.datamodel.*;
 import de.freerider.repository.CustomerRepository;
 import de.freerider.repository.ReservationRepository;
 import de.freerider.repository.VehicleRepository;
@@ -39,17 +39,46 @@ public class AppApplication {
 
     @EventListener(ApplicationReadyEvent.class)
     public void runAfterSpringStartup() {       // runs when Spring is ready
-        customerRepository.save(new Customer()
+        Customer eric = customerRepository.save(new Customer()
                 .setId(1).setName("Eric", "Meyer").addContact("eric98@yahoo.com")
                 .addContact("(030) 7000‐640000")    // updated phone number
         );
-        customerRepository.save(new Customer()
+        Customer anne = customerRepository.save(new Customer()
                 .setId(2).setName("Anne", "Bayer").addContact("anne24@yahoo.de")
                 .addContact("(030) 3481‐23352")
         );
-        customerRepository.save(new Customer()
+        Customer tim = customerRepository.save(new Customer()
                 .setId(3).setName("Tim", "Schulz‐Mueller").addContact("tim2346@gmx.de")
         );
+
+
+        Vehicle v1 = new Vehicle(476562, "Tesla", "Model 3", "red").setPower(Power.electric);
+        Vehicle v2 = new Vehicle(934773, "Tesla", "Model 3", "white").setPower(Power.electric);
+        Vehicle v3 = new Vehicle(347456, "Tesla", "Model X", "silver").setPower(Power.electric);
+        Vehicle v4 = new Vehicle(126467, "BMW", "320i", "blue");
+        Vehicle v5 = new Vehicle(356714, "BMW", "320i", "silver");
+        Vehicle v6 = new Vehicle(247245, "BMW", "320d", "red").setPower(Power.diesel);
+        Vehicle v7 = new Vehicle(247245, "VW", "ID Buzz", "red").setPower(Power.electric).setPassengers(8);
+        Vehicle v8 = new Vehicle(247245, "VW", "ID Buzz", "yellow").setPower(Power.electric).setPassengers(8);
+        //
+//			vehicleRepository.saveAll( Arrays.asList( v1, v2, v3, v4, v5, v6, v7, v8 ) );
+        //
+        Reservation r1 = new Reservation(239862, eric, v1)
+                .pickup(LocCode.BTH11, "Feb 18, 2022 10:00 UTC+0100")
+                .dropoff(LocCode.BER, "Feb 22, 2022 20:00 UTC+0100");
+        Reservation r2 = new Reservation(262510, eric, v2)
+                .pickup(LocCode.MUC, "Feb 18, 2022 10:00 UTC+0100")
+                .dropoff(LocCode.MUC, "Feb 22, 2022 20:00 UTC+0100");
+        Reservation r3 = new Reservation(342514, tim, v7)
+                .pickup(LocCode.BENT02, "Jun 12, 2022 08:00 UTC+0100")
+                .dropoff(LocCode.BER, "Jun 30, 2022 22:30 UTC+0100");
+        Reservation r4 = new Reservation(651230, anne, v8)
+                .pickup(LocCode.HAM, "Apr 17, 2022 06:00 UTC+0100")
+                .dropoff(LocCode.HAM, "Apr 18, 2022 23:00 UTC+0100");
+        Reservation r5 = new Reservation(635621, anne, v4)
+                .pickup(LocCode.BENT02, "Dec 20, 2022 10:00 UTC+0100")
+                .dropoff(LocCode.BTH11, "Jan 02, 2023 20:00 UTC+0100");
+
         long count = customerRepository.count();   // 3 customers added to repository
         System.out.println("repository<Customer> with: " + count + " entries");
     }
--- src/main/java/de/freerider/datamodel/Reservation.java
+++ src/main/java/de/freerider/datamodel/Reservation.java
@@ -1,13 +1,15 @@
 package de.freerider.datamodel;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
 
 /**
  * Class for entity type Reservation. Reservation is an state of a vehicle that belongs to a customer
  *
- * @since "0.1.0"
- * @version "0.1.0"
  * @author ksachs
+ * @version "0.1.0"
+ * @since "0.1.0"
  */
 @Entity
 @Table(name = "RESERVATION")
@@ -16,8 +18,113 @@ public class Reservation {
      * id attribute, {@code < 0} invalid, can be set only once.
      */
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="ID")
+    @Column(name = "ID")
     private int id = -1;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id")
+    private Customer owner;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vehicle_id")
+    private Vehicle vehicle;
+
+    @Column
+    private String reservation_begin;
+
+    @Column
+    private String reservation_end;
+
+    @Column
+    private LocCode reservation_pickup;
+
+    @Column
+    private String reservation_drop;
+
+    @Column
+    private String status;
+
+    public Reservation(Integer id, Customer owner, Vehicle v) {
+        this.id = id;
+        this.owner = owner;
+        this.vehicle = v;
+    }
+
+    public Customer getOwner() {
+        return owner;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getReservation_begin() {
+        return reservation_begin;
+    }
+
+    public String getReservation_end() {
+        return reservation_end;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocCode getReservation_pickup() {
+        return reservation_pickup;
+    }
+
+    public String getReservation_drop() {
+        return reservation_drop;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setOwner(Customer owner) {
+        this.owner = owner;
+    }
+
+    public void setReservation_begin(String reservation_begin) {
+        this.reservation_begin = reservation_begin;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public void setReservation_drop(String reservation_drop) {
+        this.reservation_drop = reservation_drop;
+    }
+
+    public void setReservation_end(String reservation_end) {
+        this.reservation_end = reservation_end;
+    }
+
+    public void setReservation_pickup(LocCode reservation_pickup) {
+        this.reservation_pickup = reservation_pickup;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Reservation pickup(LocCode location, String datetime) {
+        this.reservation_pickup = location;
+        this.reservation_begin = datetime;
+        return this;
+    }
+
+    public Reservation dropoff(LocCode location, String datetime) {
+        this.reservation_pickup = location;
+        this.reservation_end = datetime;
+        return this;
+    }
 
 }
