009036e888b7537a7a64f5692048eb6fb0dadc73
katharina <katharina.sachs@seerene.com>
2022-01-22 15:50:50 +0100

    wip

--- src/main/java/de/freerider/restapi/CustomersDTOAPI.java
+++ src/main/java/de/freerider/restapi/CustomersDTOAPI.java
@@ -1,74 +1,34 @@
 package de.freerider.restapi;
 
-import java.util.List;
-import java.util.Map;
-
+import de.freerider.restapi.dto.CustomerDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import io.swagger.annotations.ApiParam;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import java.util.List;
 
 
-/**
- * Spring Controller interface for /customers REST endpoint to access the
- * collection of customer resources maintained in a CustomerRepository.
- *
- * Operations provided by the endpoint:
- *
- * - GET /customers			- return JSON data for all customer in the repository,
- * 							  status: 200 OK.
- *
- * - GET /customers/{id}	- return JSON data for customer with id,
- * 							  status: 200 OK, 404 not found.
- *
- * - POST /customers		- create new objects in the repository from JSON objects
- * 							  passed with the request,
- * 							  status: 201 created, 409 conflict, 400 bad request.
- *
- * - PUT /customers			- updated existing objects in the repository from JSON
- * 							  objects passed with the request,
- * 							  status: 202 accepted, 404 not found, 400 bad request.
- *
- * - DELETE /customers/{id}	- delete customer with id,
- * 							  status: 202 accepted, 404 not found, 400 bad request.
- *
- * @author sgra64
- *
- */
-
-/*
- * Refer to endpoint URL from swagger.properties, replaces: "/api/v1/customers"
- */
-@RequestMapping( "${app.api.endpoints.customers}" )
-//
-public interface CustomersAPI {
-
-
-    /**
-     * GET /customers
-     *
-     * @return JSON Array with customers (compact).
-     */
+@RequestMapping("${app.api.endpoints.customers}")
+public interface CustomersDTOAPI {
 
     /*
      * Swagger API doc annotations:
      */
     @Operation(
-            summary = "Return all customers from repository.",		// appears as text in API short-list
-            description = "Return all customers from repository.",	// appears as text inside API
-            tags={ "customers-controller" }	// appears in swagger-ui URL: http://localhost:8080/swagger-ui/index.html#/customers-controller
+            summary = "Return all customers from repository.",        // appears as text in API short-list
+            description = "Return all customers from repository.",    // appears as text inside API
+            tags = {"customers-controller"}    // appears in swagger-ui URL: http://localhost:8080/swagger-ui/index.html#/customers-controller
     )
-    @ApiResponses( value = {	// also auto-derived by Swagger
-            @ApiResponse( responseCode = "200", description = "OK", content = @Content(mediaType = "application/json")),
-            @ApiResponse( responseCode = "401", description = "Unauthorized"),
-            @ApiResponse( responseCode = "403", description = "Forbidden"),
+    @ApiResponses(value = {    // also auto-derived by Swagger
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
             // to remove "404" from docs, set SwaggerConfig::Docket.useDefaultResponseMessages(true) // ->false
 //		@ApiResponse( responseCode = "404", description = "Not Found")
     })
@@ -78,11 +38,10 @@ public interface CustomersAPI {
      */
     @RequestMapping(
             method = RequestMethod.GET,
-            value = "",	// relative to interface @RequestMapping
-            produces = { "application/json" }
+            value = "",    // relative to interface @RequestMapping
+            produces = {"application/json"}
     )
-    //
-    ResponseEntity<List<?>> getCustomers();
+    ResponseEntity<List<CustomerDTO>> getCustomers();
 
 
     /**
@@ -97,35 +56,31 @@ public interface CustomersAPI {
     @Operation(
             summary = "Return customer with {id} from repository.",
             description = "Return customer with {id} from repository.",
-            tags={ "customers-controller" }
+            tags = {"customers-controller"}
     )
 
     /*
      * Spring REST Controller annotation:
      */
     @RequestMapping(
-            method=RequestMethod.GET,
-            value="{id}",	// relative to interface @RequestMapping
-            produces={ "application/json" }
+            method = RequestMethod.GET,
+            value = "{id}",    // relative to interface @RequestMapping
+            produces = {"application/json"}
     )
     //
-    ResponseEntity<?> getCustomer(
-            @PathVariable("id")
-            @ApiParam(value = "Customer id", required = true)
-                    long id
-    );
+    ResponseEntity<CustomerDTO> getCustomer( @PathVariable("id") long id );
 
 
     /**
      * POST /customers
-     *
+     * <p>
      * Add new customers from JSON data passed as array of JSON objects in the request body.
      * Multiple customers can be posted with multiple JSON objects from the same request.
      * Id's are assigned, if id-attributes are missing or are empty in JSON data.
-     *
+     * <p>
      * JSON data containing id of objects that are already present are rejected. Rejected
      * objects are returned in the response with error 409 (conflict).
-     *
+     * <p>
      * Status 201 (created) is returned with empty array of conflicts when all objects were
      * accepted. Partial acceptance of objects from the request is possible, but error 409 is
      * returned with the array of rejected objects.
@@ -140,7 +95,7 @@ public interface CustomersAPI {
     @Operation(
             summary = "Add new customers to repository.",
             description = "Add new customers to repository.",
-            tags={ "customers-controller" }
+            tags = {"customers-controller"}
     )
 
     /*
@@ -148,21 +103,20 @@ public interface CustomersAPI {
      */
     @RequestMapping(
             method = RequestMethod.POST,
-            value = ""	// relative to interface @RequestMapping
+            value = ""    // relative to interface @RequestMapping
     )
     //
-    ResponseEntity<List<?>> postCustomers( @RequestBody Map<String, Object>[] jsonMap  );
-
+    ResponseEntity<List<CustomerDTO>> postCustomers( @RequestBody List<CustomerDTO> dtos );
 
     /**
      * PUT /customers
-     *
+     * <p>
      * Update existing customers from JSON data passed as array of JSON objects in the request body.
      * Multiple customers can be updated from multiple JSON objects from the same request.
-     *
+     * <p>
      * JSON data missing id or with id that are not found are rejected. Rejected JSON objects
      * are returned in the response with error 404 (not found).
-     *
+     * <p>
      * Status 202 (accepted) is returned with empty array of conflicts when all updates could be
      * performed. Partial acceptance of updates is possible for entire objects only (not single
      * attributes). Error 409 (conflict) is returned for errors other than an object (id) was not
@@ -178,7 +132,7 @@ public interface CustomersAPI {
     @Operation(
             summary = "Update existing customers in repository.",
             description = "Update existing customers in repository.",
-            tags={ "customers-controller" }
+            tags = {"customers-controller"}
     )
 
     /*
@@ -186,23 +140,10 @@ public interface CustomersAPI {
      */
     @RequestMapping(
             method = RequestMethod.PUT,
-            value = ""	// relative to interface @RequestMapping
+            value = ""    // relative to interface @RequestMapping
     )
     //
-    public ResponseEntity<List<?>> putCustomers( @RequestBody Map<String, Object>[] jsonMap );
-
-
-    /**
-     * DELETE /customers/{id}
-     *
-     * Delete existing customer by its id. A missing id or an id that was not found
-     * returns error 404 (not found).
-     *
-     * Status 202 (accepted) is returned with successful completion of the operation.
-     *
-     * @param id id of object to delete.
-     * @return status code: 202 (accepted), 404 (not found).
-     */
+    ResponseEntity<List<CustomerDTO>> putCustomers( @RequestBody List<CustomerDTO> dtos );
 
     /*
      * Swagger API doc annotations:
@@ -210,7 +151,7 @@ public interface CustomersAPI {
     @Operation(
             summary = "Delete customer by its id from repository.",
             description = "Delete customer by its id from repository.",
-            tags={ "customers-controller" }
+            tags = {"customers-controller"}
     )
 
     /*
@@ -218,16 +159,8 @@ public interface CustomersAPI {
      */
     @RequestMapping(
             method = RequestMethod.DELETE,
-            value = "{id}"	// relative to interface @RequestMapping
+            value = "{id}"    // relative to interface @RequestMapping
     )
     //
-    public ResponseEntity<?> deleteCustomer( @PathVariable("id") long id );
-
-
-    /*
-     * Alternative using DTO serialization.
-     */
-//	@RequestMapping( method = RequestMethod.POST, value = "" )
-//	public ResponseEntity<List<?>> postCustomers( @RequestBody CustomerJsonDTO[] dtos );
-
+    ResponseEntity<?> deleteCustomer( @PathVariable("id") long id );
 }
---
+++ src/main/java/de/freerider/restapi/CustomersDTOController.java
@@ -0,0 +1,124 @@
+package de.freerider.restapi;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.freerider.datamodel.Customer;
+import de.freerider.repository.CustomerRepository;
+import de.freerider.restapi.dto.CustomerDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.mappers.ModelMapper;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+
+@RestController
+public class CustomersDTOController implements CustomersDTOAPI {
+    @Autowired
+    private ModelMapper modelMapper;
+    @Autowired
+    private ApplicationContext context;
+    @Autowired
+    CustomerRepository repo;
+    private final ObjectMapper objectMapper;
+    private final HttpServletRequest request;
+
+    /**
+     * Constructor.
+     *
+     * @param objectMapper entry point to JSON tree for the Jackson library
+     * @param request      HTTP request object
+     */
+    public CustomersDTOController(ObjectMapper objectMapper, HttpServletRequest request) {
+        this.objectMapper = objectMapper;
+        this.request = request;
+    }
+
+    @Override
+    public ResponseEntity<List<CustomerDTO>> getCustomers() {
+        ResponseEntity<List<CustomerDTO>> response = null;
+        System.err.println(request.getMethod() + " " + request.getRequestURI());
+        Iterable<Customer> cList = repo.findAll();
+        List<CustomerDTO> dtoList = new ArrayList<CustomerDTO>();
+        cList.forEach(c -> {
+            dtoList.add(new CustomerDTO(c));
+        });
+        response = new ResponseEntity<List<CustomerDTO>>(dtoList, HttpStatus.OK);
+        return response;
+    }
+
+    @Override
+    public ResponseEntity<CustomerDTO> getCustomer(long id) {
+        ResponseEntity<CustomerDTO> response = null;
+        System.err.println(request.getMethod() + " " + request.getRequestURI());
+        Optional c = repo.findById(id);
+        if (c.isPresent()) {
+            Customer customer = (Customer) c.get();
+            CustomerDTO dto = new CustomerDTO(customer);
+            response = new ResponseEntity<CustomerDTO>(dto, HttpStatus.OK);
+        } else {
+            System.err.println(request.getMethod() + " " + request.getRequestURI());
+            response = new ResponseEntity<CustomerDTO>(HttpStatus.NOT_FOUND);
+            System.err.println(response);
+        }
+        return response;
+    }
+
+    @Override
+    public ResponseEntity<List<CustomerDTO>> postCustomers(List<CustomerDTO> dtos) {
+        System.err.println("POST /customers");
+
+        if (dtos == null)
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+
+        List<Customer> cList = new ArrayList<Customer>();
+        for (CustomerDTO dto : dtos) {
+            Optional<Customer> c = dto.create();
+            if (c.isPresent()) {
+                Customer customer = c.get();
+                cList.add(customer);
+            } else {
+                return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            }
+        }
+        repo.saveAll(cList);
+        return new ResponseEntity<List<CustomerDTO>>(dtos, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<List<CustomerDTO>> putCustomers(List<CustomerDTO> dtos) {
+        if (dtos == null)
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+
+        List<Customer> cList = new ArrayList<Customer>();
+        for (CustomerDTO dto : dtos) {
+            Optional<Customer> c = dto.create();
+            if (c.isPresent()) {
+                Customer customer_new = c.get();
+                Optional customer = repo.findById(customer_new.getId());
+                if (customer.isPresent()) {
+                    Customer current_customer = (Customer) customer.get();
+                    current_customer.setId(customer_new.getId());
+                    current_customer.setName(customer_new.getFirstName(), customer_new.getLastName());
+                    repo.save(current_customer);
+                }
+            }
+        }
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<?> deleteCustomer(long id) {
+        System.err.println("DELETE /customers/" + id);
+        if (repo.existsById(id)) {
+            repo.deleteById(id);
+            System.out.println("customer " + id + " deleted.");
+            return new ResponseEntity<>(null, HttpStatus.ACCEPTED); // status 202
+        } else {
+            System.err.println("customer " + id + " not found.");
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND); // status 404
+        }
+    }
+
+}
